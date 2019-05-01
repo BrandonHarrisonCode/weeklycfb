98af6ee730d67ba97fdd0ebe20adcc204efa9a92
Andrew McAdams <andmcadams@gmail.com>
2019-05-01 18:06:59 -0500

    Remove test files

--- ExpectedPoints/handler.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def my_func(event, context): 
-    return {
-        'statusCode': 200,
-        'headers': { 'Content-Type': 'application/json' },
-        'body': json.dumps({ 'username': 'thisistheusername', 'id': 12 })
-    }
