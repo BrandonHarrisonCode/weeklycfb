66cc4987b52c3de769ee8ed1598339d14961f0da
Brandon Harrison <brandonharrisoncode@gmail.com>
2019-05-02 20:56:28 -0500

    Update lambda

---
+++ ComputeScoreLambda/computescore.py
@@ -0,0 +1,57 @@
+import requests
+import json
+import urllib
+from winprobability import play_win_probability, pregame_win_probability, postgame_win_probability, moving_average
+from operator import itemgetter
+
+def compute_score(game):
+    url = create_plays_url(game)
+    home_team = game['home_team']
+    vegas_line = 0
+
+    plays = get_plays(url)
+    win_probabilites = compute_win_probabilities(plays, home_team, vegas_line)
+    smoothed = get_smoothed_data(win_probabilites, home_team, vegas_line, plays)
+    print([[x, smoothed[x]] for x in range(0, len(smoothed))])
+
+    distance = get_distance(smoothed)
+    print(distance)
+    average_distance = distance / len(smoothed)
+    return average_distance
+
+
+def create_plays_url(game):
+    params = {'year': game['season'], 'week': game['week'], 'team': game['home_team']}
+    base = 'https://api.collegefootballdata.com/plays?seasonType=both&'
+    return base + urllib.parse.urlencode(params)
+
+
+def get_plays(url):
+    result = requests.get(url)
+    if not result.ok:
+        raise IOError('Could not access API')
+    plays = json.loads(result.content)
+    return sorted(plays, key=itemgetter('id'))
+
+
+def compute_win_probabilities(plays, home_team, vegas_line):
+    win_probabilites = []
+    for play in plays:
+        win_probability = 1 - play_win_probability(home_team, vegas_line, play)
+        if win_probability is not None:
+            win_probabilites.append(win_probability)
+    return win_probabilites
+
+
+def get_smoothed_data(win_probabilites, home_team, vegas_line, plays):
+    smoothed = moving_average(win_probabilites, 3)
+    smoothed.insert(0, 1 - pregame_win_probability(home_team, vegas_line))
+    smoothed.append(1 - postgame_win_probability(home_team, plays[-1]))
+    return smoothed
+
+
+def get_distance(array):
+    distance = 0
+    for i in range(1, len(array)):
+        distance += abs(array[i] - array[i - 1])
+    return distance
--- ComputeScoreLambda/lambda_function.py
+++ ComputeScoreLambda/lambda_function.py
@@ -1,35 +1,24 @@
 import requests
 import json
-import scipy.signal
-from winprobability import play_win_probability, pregame_win_probability, postgame_win_probability, moving_average
+from computescore import compute_score
 from operator import itemgetter
 
 def lambda_handler(event, context):
-    plays_url = 'https://api.collegefootballdata.com/plays?seasonType=regular&year=2018&week=13&team=LSU'
-    result = requests.get(plays_url)
-    if not result.ok:
-        return abort(500)
+    url = 'https://api.collegefootballdata.com/games?year=2018&week=13&seasonType=both'
+    games = get_games(url)
 
-    home_team = 'Texas A&M'
-    vegas_line = -3.0
-
-    plays = json.loads(result.content)
-    # plays = filter(lambda i: len(i['clock']) != 0, plays)
-    plays = sorted(plays, key=itemgetter('id'))
-    win_probabilites = []
-    count = 0
-    for play in plays:
-        print(play)
-        win_probability = 1 - play_win_probability(home_team, vegas_line, play)
-        print(win_probability)
-        if win_probability is not None:
-            win_probabilites.append(win_probability)
-            count += 1
-    smoothed = moving_average(win_probabilites, 3)
-    smoothed.insert(0, 1 - pregame_win_probability(home_team, vegas_line))
-    smoothed.append(1 - postgame_win_probability(home_team, plays[-1]))
-    print([[x, win_probabilites[x]] for x in range(0, len(win_probabilites))])
-    print([[x, smoothed[x]] for x in range(0, len(smoothed))])
+    # games = filter(lambda x: x['away_team'] == 'LSU', games)
+    scores = []
+    for game in games:
+        try:
+            title = '{} vs. {}'.format(game['home_team'], game['away_team'])
+            print(title)
+            score = compute_score(game)
+            print('Final score: {}\n\n'.format(score))
+            scores.append({'title': title, 'score': score})
+        except Exception as e:
+            print(e)
+    print(sorted(scores, key=itemgetter('score'), reverse=True))
 
     return {
         'statusCode': 200
@@ -40,6 +29,24 @@ def abort(statuscode):
     return {
         'statusCode': statuscode
     }
+
+
+def get_games(url):
+    result = requests.get(url)
+    if not result.ok:
+        raise IOError('Could not access API')
+    data = json.loads(result.content)
+    games = []
+    for game in data:
+        games.append({
+            'season': game['season'],
+            'week': game['week'],
+            'home_team': game['home_team'],
+            'away_team': game['away_team'],
+            'home_points': game['home_points'],
+            'away_points': game['away_points'],
+        })
+    return games
 
 
 if __name__ == '__main__':
