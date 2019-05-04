e6ef28f32cbf182228a2b41d81fbfbcf78a7d9f2
Andrew McAdams <andmcadams@gmail.com>
2019-05-04 12:59:55 -0500

    Add DecimalEncoder from docs to produce valid json output

--- accessModule/lambda_function.py
+++ accessModule/lambda_function.py
@@ -1,8 +1,21 @@
 import boto3
+import json
+import decimal
+
 from boto3.dynamodb.conditions import Key, Attr
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('CalculatedScores')
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if abs(o) % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
 
 """
 This function returns a list of the games for a given season and week.
@@ -14,8 +27,8 @@ def lambda_handler(event, context):
 	# Remove play by play score data from the response.
 	filtered_response = [{key:game_dict[key] for key in game_dict if key != 'play-by-play'} for game_dict in response['Items']]
 	return {
+	  'isBase64Encoded': False,
       'statusCode': 200,
       'headers': {},
-      'body': "Required body for api gateway.",
-      'data': filtered_response    
+      'body': json.dumps(filtered_response, cls=DecimalEncoder)  
     }
