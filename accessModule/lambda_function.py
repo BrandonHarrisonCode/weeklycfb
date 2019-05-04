e7618bd36b55e7dbd56f6e6149928ac4c17c8272
Andrew McAdams <andmcadams@gmail.com>
2019-05-04 09:16:27 -0500

    See the format of response

--- accessModule/lambda_function.py
+++ accessModule/lambda_function.py
@@ -3,11 +3,15 @@ from boto3.dynamodb.conditions import Key, Attr
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('CalculatedScores')
+
+"""
+This function returns a list of the games for a given season and week.
+"""
 def lambda_handler(event, context):
 	response = table.query(
     KeyConditionExpression=Key('year:week').eq("2018:13")
 	)
 	return {
       'statusCode': 200,
-      'body': response['Items'][0]['year:week']
+      'data': response['Items']
   }
