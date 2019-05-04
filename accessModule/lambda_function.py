9ade3fb82506453c9c78c0095b0ab62898d80d13
Andrew McAdams <andmcadams@gmail.com>
2019-05-04 09:43:44 -0500

    Correct renaming and add required response keys

--- accessModule/lambda_function.py
+++ accessModule/lambda_function.py
@@ -12,8 +12,10 @@ def lambda_handler(event, context):
     KeyConditionExpression=Key('year:week').eq("2018:13")
 	)
 	# Remove play by play score data from the response.
-	filtered_response = [{key:game_dict[field] for key in game_dict if key != 'play-by-play'} for game_dict in response['Items']]
+	filtered_response = [{key:game_dict[key] for key in game_dict if key != 'play-by-play'} for game_dict in response['Items']]
 	return {
       'statusCode': 200,
+      'headers': {},
+      'body': "Required body for api gateway.",
       'data': filtered_response    
     }
