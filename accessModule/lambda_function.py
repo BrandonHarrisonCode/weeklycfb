a8d8685e8b392fd323530c57ea34992b2899da0c
Andrew McAdams <andmcadams@gmail.com>
2019-05-05 16:31:11 -0500

    Add ACAO header to avoid CORS issues with frontend

--- accessModule/lambda_function.py
+++ accessModule/lambda_function.py
@@ -9,28 +9,30 @@ table = dynamodb.Table('CalculatedScores')
 
 # Helper class to convert a DynamoDB item to JSON.
 class DecimalEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, decimal.Decimal):
-            if abs(o) % 1 > 0:
-                return float(o)
-            else:
-                return int(o)
-        return super(DecimalEncoder, self).default(o)
+		def default(self, o):
+				if isinstance(o, decimal.Decimal):
+						if abs(o) % 1 > 0:
+								return float(o)
+						else:
+								return int(o)
+				return super(DecimalEncoder, self).default(o)
 
 """
 This function returns a list of the games for a given season and week.
 """
 def lambda_handler(event, context):
 	# This call should be coming from an AWS API-Gateway. Might want to find some way to check for this and fail otherwise.
-  yearweek = event['queryStringParameters']['year'] + ":" + event['queryStringParameters']['week']
-  response = table.query(
-      KeyConditionExpression=Key('year:week').eq(yearweek)
+	yearweek = event['queryStringParameters']['year'] + ":" + event['queryStringParameters']['week']
+	response = table.query(
+			KeyConditionExpression=Key('year:week').eq(yearweek)
 	)
 	# Remove play by play score data from the response.
-  filtered_response = [{key:game_dict[key] for key in game_dict if key != 'play-by-play'} for game_dict in response['Items']]
-  return {
-    'isBase64Encoded': False,
-      'statusCode': 200,
-      'headers': {},
-      'body': json.dumps(filtered_response, cls=DecimalEncoder)  
-  }
+	filtered_response = [{key:game_dict[key] for key in game_dict if key != 'play-by-play'} for game_dict in response['Items']]
+	return {
+		'isBase64Encoded': False,
+		'statusCode': 200,
+		'headers': {            
+			"Access-Control-Allow-Origin": "*"
+		},
+		'body': json.dumps(filtered_response, cls=DecimalEncoder)  
+	}
