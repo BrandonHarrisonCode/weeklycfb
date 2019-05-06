2179f9335889c4a8246f793bf24beb60c9f77eb1
Andrew McAdams <andmcadams@gmail.com>
2019-05-06 14:33:29 -0500

    Add ability to return play-by-play data for a specific game

--- accessModule/lambda_function.py
+++ accessModule/lambda_function.py
@@ -17,27 +17,55 @@ class DecimalEncoder(json.JSONEncoder):
 								return int(o)
 				return super(DecimalEncoder, self).default(o)
 
-"""
-This function returns a list of the games for a given season and week.
-"""
-def lambda_handler(event, context):
-	# This call should be coming from an AWS API-Gateway. Might want to find some way to check for this and fail otherwise.
-	if not event['queryStringParameters'] or not event['queryStringParameters']['year'] or not event['queryStringParameters']['week']:
-		abort("Bad Request", 400);
-	yearweek = event['queryStringParameters']['year'] + ":" + event['queryStringParameters']['week']
+def retrieve_play_by_play(yearweek, score):
 	response = table.query(
-			KeyConditionExpression=Key('year:week').eq(yearweek),
-			ProjectionExpression="score,home,away",
+			ExpressionAttributeNames={"#pbp":"play-by-play"},
+			KeyConditionExpression=Key('year:week').eq(yearweek) & Key('score').eq(decimal.Decimal(score)),
+			ProjectionExpression="#pbp,home",
 			ScanIndexForward=False,
 			Select="SPECIFIC_ATTRIBUTES"
-	)
+	)	
+	built_response = {
+		'yearweek': yearweek,
+		'data': response['Items']
+	}
 	return {
 		'isBase64Encoded': False,
 		'statusCode': 200,
 		'headers': {            
 			"Access-Control-Allow-Origin": "*"
 		},
-		'body': json.dumps(response['Items'], cls=DecimalEncoder)  
+		'body': json.dumps(built_response, cls=DecimalEncoder)  
+	}
+"""
+This function returns a list of the games for a given season and week.
+"""
+def lambda_handler(event, context):
+	# This call should be coming from an AWS API-Gateway. Might want to find some way to check for this and fail otherwise.
+	if not 'queryStringParameters' in event or not all (key in event['queryStringParameters'] for key in ('year', 'week')):
+		abort("Bad Request", 400);
+	yearweek = event['queryStringParameters']['year'] + ":" + event['queryStringParameters']['week']
+	if 'score' in event['queryStringParameters']:
+		return retrieve_play_by_play(yearweek, event['queryStringParameters']['score'])
+	response = table.query(
+			KeyConditionExpression=Key('year:week').eq(yearweek),
+			ProjectionExpression="score,home,away",
+			ScanIndexForward=False,
+			Select="SPECIFIC_ATTRIBUTES"
+	)
+
+	built_response = {
+		'yearweek': yearweek,
+		'data': response['Items']
+	}
+
+	return {
+		'isBase64Encoded': False,
+		'statusCode': 200,
+		'headers': {            
+			"Access-Control-Allow-Origin": "*"
+		},
+		'body': json.dumps(built_response, cls=DecimalEncoder)  
 	}
 
 def abort(message, code):
