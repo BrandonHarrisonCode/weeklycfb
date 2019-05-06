ddc558e32eb79b7d61f35cce1690934a6487d177
Brandon Harrison <brandonharrisoncode@gmail.com>
2019-05-05 19:24:47 -0500

    Working SAM template

--- upload.sh
+++ upload.sh
@@ -2,16 +2,7 @@
 
 set -e
 
-lambda_folder_suffix=Module
-lambdas=$(find . -maxdepth 1 -name "*${lambda_folder_suffix}" -print)
-for folder in ${lambdas}
-do
-    basename=$(basename ${folder})
-    cd ${basename}
-    cd venv/lib/python3.7/site-packages/
-    zip -r9 ../../../../function.zip .
-    cd ../../../../
-    zip -g function.zip *.py
-    aws s3 --profile cfb cp function.zip s3://cfb-game-of-the-week-zip-files/function-${basename}.zip
-    cd ..
-done
+sam validate
+sam build
+sam package --output-template-file packaged.yml --s3-bucket cfb-game-of-the-week-zip-files
+sam deploy --template-file packaged.yml --stack-name CloudFormation --capabilities CAPABILITY_IAM --region us-east-1 --parameter-overrides DeploymentStage=Dev
