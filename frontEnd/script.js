bd51d74a6bf40052302ce5f4853f51f70adc6525
Andrew McAdams <andmcadams@gmail.com>
2019-05-06 16:14:46 -0500

    Create a graph when clicking on game

--- frontEnd/script.js
+++ frontEnd/script.js
@@ -2,6 +2,8 @@ const factor = 10
 
 const app = document.getElementById('root')
 
+var yearweek = null
+
 //Create dropdown menus
 const year_dropdown = document.getElementById('year_dropdown')
 const week_dropdown = document.getElementById('week_dropdown')
@@ -30,6 +32,56 @@ function calculate_new_score(top_score, score) {
   return 100*Math.pow(factor, 1+15*score)/top_score
 }
 
+function create_linechart(game_element_id) {
+  var split_gid = game_element_id.split(':')
+  console.log("yearweek = " + yearweek)
+  console.log("game.score = " + split_gid[0])
+  console.log("game.away = " + split_gid[1])
+  var split_ywk = yearweek.split(':')
+  var request = new XMLHttpRequest()
+
+  request.open('GET', 'https://fchv75rdm1.execute-api.us-east-1.amazonaws.com/default/travisAccessLambda?year=' + split_ywk[0] + '&week=' + split_ywk[1] + '&score=' + split_gid[0], true)
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) { 
+      var response = JSON.parse(this.response)
+      var data = response.data
+      yearweek = response.yearweek
+
+      google.charts.load('current', {packages: ['corechart', 'line']});
+      google.charts.setOnLoadCallback(drawBasic);
+
+      function drawBasic() {
+        var table = new google.visualization.DataTable();
+        table.addColumn('number', 'Plays');
+        table.addColumn('number', `Probability of ${split_gid[1]} Win`);
+
+        var arr = data[0]['play-by-play']
+        var prob_array = arr.map(function(elem, index) {
+          if(index == 0)
+            return null
+          return [index, elem]
+        })
+        console.log(prob_array)
+        table.addRows(prob_array);
+
+        var options = {
+          hAxis: {
+            title: 'Plays'
+          },
+          vAxis: {
+            title: `Probability of ${split_gid[1]} Win`
+          }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+        chart.draw(table, options);
+      }
+    }
+  }
+  request.send()
+}
+
 function retrieve_data() {
   var request = new XMLHttpRequest()
   request.open('GET', 'https://fchv75rdm1.execute-api.us-east-1.amazonaws.com/default/travisAccessLambda?year=' + year_dropdown.value + '&week=' + week_dropdown.value, true)
@@ -42,7 +94,9 @@ function retrieve_data() {
 
     if (request.status >= 200 && request.status < 400) {
       // Begin accessing JSON data here
-      var data = JSON.parse(this.response)
+      var response = JSON.parse(this.response)
+      var data = response.data
+      yearweek = response.yearweek
 
 
       if(data != "") {
@@ -52,12 +106,14 @@ function retrieve_data() {
         data.forEach(game => {
 
           const li = document.createElement('li')
+          li.onclick = function() { create_linechart(this.id) }
           const span = document.createElement('span')
           span.textContent = num++
 
           const p = document.createElement('p')
 
           var score = calculate_new_score(top_score, game.score)
+          li.id = `${game.score}:${game.home}`
 
           p.textContent = `${game.away} vs. ${game.home} : ${score}`
 
