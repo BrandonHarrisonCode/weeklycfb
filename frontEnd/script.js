3b93b640c324598dc3c2c4fc1ab84499490505de
Andrew McAdams <andmcadams@gmail.com>
2019-05-06 20:57:22 -0500

    Add away attribute to chart_div to check that the correct game is selected

--- frontEnd/script.js
+++ frontEnd/script.js
@@ -21,16 +21,22 @@ for(var i = 1; i < 17; i++) {
 }
 
 
-function create_linechart(score) {
+function create_linechart(score, awayTeamName) {
   var listOfGames = document.getElementById('list_of_games')
+  var chartDiv = document.getElementById('chart_div')
+  var request = new XMLHttpRequest()
 
   // TODO Return some error if listOfGames cannot be found
-  if(listOfGames != null) {
-    var request = new XMLHttpRequest()
+  if(chartDiv != null) {
+    // Set this so we can make sure that we picked the right game later. 
+    // Used in the unlikely case two games have the same score.
+    chartDiv.setAttribute('away', awayTeamName)
+    if(listOfGames != null) {
 
-    request.open('GET', lambdaURL + '?year=' + listOfGames.getAttribute('year') + '&week=' + listOfGames.getAttribute('week') + '&score=' + score, true)
-    request.onload = function() { onload_wrapper(onload_generate_graph, request, this.response) }
-    request.send()
+      request.open('GET', lambdaURL + '?year=' + listOfGames.getAttribute('year') + '&week=' + listOfGames.getAttribute('week') + '&score=' + score, true)
+      request.onload = function() { onload_wrapper(onload_generate_graph, request, this.response) }
+      request.send()
+    }
   }
 }
 
