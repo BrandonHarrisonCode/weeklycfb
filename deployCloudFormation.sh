656dd6841473f59e5b05c0e41535f4a5a0a26074
Andrew McAdams <andmcadams@gmail.com>
2019-05-07 17:09:06 -0500

    Refactor to set environment variables

--- deployCloudFormation.sh
+++ deployCloudFormation.sh
@@ -2,11 +2,13 @@ set -e
 
 if [ $TRAVIS_BRANCH -eq "master" ]
 then
-  STACKNAME="ProductionStack"
-  DEPLOYMENTSTAGE="Production"
+  STACK_NAME="ProductionStack"
+  DEPLOYMENT_STAGE="Production"
+  FOLDER_NAME="ProductionWebResources"
 else
-  STACKNAME="${TRAVIS_BRANCH}Stack"
-  DEPLOYMENTSTAGE="Dev"
+  STACK_NAME="${TRAVIS_BRANCH}Stack"
+  DEPLOYMENT_STAGE="Dev"
+  FOLDER_NAME="${TRAVIS_BRANCH}WebResources"
 fi
 
 aws --version
@@ -14,15 +16,15 @@ sam --version
 sam validate
 sam build --use-container
 sam package --output-template-file packaged.yml --s3-bucket cfb-game-of-the-week-zip-files
-sam deploy --template-file packaged.yml --stack-name $STACKNAME --capabilities CAPABILITY_IAM --region us-east-1 --parameter-overrides DeploymentStage=$DEPLOYMENTSTAGE
+sam deploy --template-file packaged.yml --stack-name ${STACK_NAME} --capabilities CAPABILITY_IAM --region us-east-1 --parameter-overrides DeploymentStage=${DEPLOYMENT_STAGE}
 
 echo "Updating script file..."
-APIURL=$(aws cloudformation describe-stacks --stack-name $STACKNAME --query Stacks[].Outputs[*].[OutputValue] --output text)
-echo "const lambdaURL = '$APIURL'" > tmpfile
+API_URL=$(aws cloudformation describe-stacks --stack-name ${STACK_NAME} --query Stacks[].Outputs[*].[OutputValue] --output text)
+echo "const lambdaURL = '${API_URL}'" > tmpfile
 sed '1d' frontEnd/script.js >> tmpfile
 rm frontEnd/script.js && mv tmpfile frontEnd/script.js
 echo "The file script.js was updated."
 cat frontEnd/script.js
 echo "SUCCESS"
 echo This build was pushed from branch ${TRAVIS_BRANCH}.
-echo The API URL is $APIURL
+echo The API URL is ${API_URL}
