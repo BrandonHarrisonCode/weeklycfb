80edf0930b11b476a6bfbf9a1c1dc132680f191b
Brandon Harrison <brandonharrisoncode@gmail.com>
2020-04-24 20:12:55 -0600

    Add loading spinner and error message for not loading the correct game

--- frontend/src/components/frontPage/cards/CardList.js
+++ frontend/src/components/frontPage/cards/CardList.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import 'typeface-roboto';
+import { withStyles } from '@material-ui/core/styles';
+import Snackbar from '@material-ui/core/Snackbar';
+import Alert from '@material-ui/lab/Alert';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import './CardList.css';
 import Card from './Card';
 
-export default class CardList extends React.Component {
+const useStyles = theme => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+});
+
+class CardList extends React.Component {
   constructor(props) {
     super(props);
+
+    this.handleErrorClose = this.handleErrorClose.bind(this);
+
     this.state = {
       loading: true,
       gamesData: [],
+      error: false,
     };
+  }
+
+  handleErrorClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    this.setState({
+      error: false
+    });
   }
 
   async componentDidMount() {
     this.setState({
       gamesData: [], 
-      loading: true
+      loading: true,
+      error: false,
     });
  
     this.fetchGames();
@@ -31,24 +57,53 @@ export default class CardList extends React.Component {
   }
 
   async fetchGames() {
-    const url = 'https://api.cfbgameoftheweek.com/entertainmentScores?week=' + this.props.week + '&year=' + this.props.year;
-    const response = await fetch(url);
-    const parsedJSON = await response.json();
-
     this.setState({
-      gamesData: parsedJSON.data, 
-      loading: false
+      loading: true
+    });
+
+    const url = 'https://api.cfbgameoftheweek.com/entertainmentScores?week=' + this.props.week + '&year=' + this.props.year;
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Error while downloading data about the week.');
+        }
+        return response.json();
+      })
+      .then((parsedJSON) => {
+        this.setState({
+          gamesData: parsedJSON.data, 
+          loading: false,
+          error: false
+        });
+      })
+    .catch((error) => {
+      console.error('Unable to access the api at:', url);
+      this.setState({
+        loading: false,
+        error: true
+      });
     });
   }
 
   render() {
-    const {loading, gamesData} = this.state;
+    const classes = this.props.classes;
+    const {loading, gamesData, error} = this.state;
+
     return (
       <Paper elevation={3} className="cardList">
         <List>
           {
-            loading ? "loading..." : 
-              gamesData.slice(0,10).map((game, rank) => {
+            loading ? 
+              <Backdrop className={classes.backdrop} open={true}>
+                <CircularProgress color="inherit" />
+              </Backdrop>
+            : error ?
+              <Snackbar open={error} autoHideDuration={6000} onClose={this.handleErrorClose}> 
+                <Alert severity="error" variant="filled" onClose={this.handleErrorClose}>
+                  Error: Unable to access the data for this week!
+                </Alert>
+              </Snackbar>
+              : gamesData.slice(0,10).map((game, rank) => {
                 const {home, away} = game;
                 return (
                   <ListItem key={rank}>
@@ -61,3 +116,5 @@ export default class CardList extends React.Component {
     );
   }
 }
+
+export default withStyles(useStyles)(CardList);
