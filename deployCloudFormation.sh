8b30c6af160e497190c647b09fca4f6d4862e965
Brandon Harrison <brandonharrisoncode@gmail.com>
2020-05-24 15:09:16 -0600

    Ask virtualenv python to run pytest

--- deployCloudFormation.sh
+++ deployCloudFormation.sh
@@ -24,7 +24,7 @@ for folder in $(find . -type d -name \*Module) ; do
   source venv/bin/activate
   python3 -m pip install -r requirements.txt
   python3 -m pip install pytest
-  pytest  
+  python3 -m pytest  
   deactivate
   cd .. 
 done
