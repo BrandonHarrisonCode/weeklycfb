f2bd838437e597e825f5d9569600e543e53a71b1
Brandon Harrison <brandonharrisoncode@gmail.com>
2020-08-27 22:45:23 -0600

    Remove unused dependencies

--- frontend/src/components/frontPage/cards/Card.js
+++ frontend/src/components/frontPage/cards/Card.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import './Card.css'
 
 export default function SimpleCard(props) {
